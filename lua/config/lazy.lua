94ffc9a7f3992a858a3f5e4e8287adf54d1a55bc
rashed parvez <parvez08eee@gmail.com>
2025-05-16 18:27:02 +0600

    Added lazygit plugin and lsp fix for typescript

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
   },
   
   defaults = {
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,62 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false }
+  -- Mason: Package manager for LSP servers
+  {
+    "williamboman/mason.nvim",
+    opts = {}
   },
+
+  -- Mason-LSPConfig: Bridge between Mason and nvim-lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls", "tsserver" }, -- Example servers
+      automatic_installation = true,
+    },
+  },
+
+  -- nvim-lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        vtsls = {
+          filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+          settings = {
+            typescript = {
+              inlayHints = {
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+              },
+            },
+          },
+        },
+      },
+      setup = {
+        vtsls = function(_, opts)
+          require("lspconfig").vtsls.setup(opts)
+        end,
+      },
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        -- Your mason-lspconfig setup here
+      })
+
+      -- Setup language servers
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+    end
+  }
 }
