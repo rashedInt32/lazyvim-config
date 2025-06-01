b3e6f6944d64d9a2e05683e893ba99de23580ce6
rashed parvez <parvez08eee@gmail.com>
2025-06-01 16:14:31 +0600

    Placed float toggleterm to bottom

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,16 +1,27 @@
 return {
-  "akinsho/toggleterm.nvim",
-  opts = {
-    open_mapping = [[<C-\>]],
-    direction = "float", -- Valid options: "float", "horizontal", "vertical", "tab"
-    float_opts = {
-      -- The border key is *almost* the same as 'nvim_open_win'
-      -- see :h nvim_open_win for details on borders however
-      -- the 'curved' border is a custom border type
-      -- not natively supported but implemented in this plugin.
-      border = "curved",
-      height = 20,
-      row = 10,
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*", -- Use latest version
+    event = "VeryLazy", -- Load with LazyVim
+    opts = {
+      open_mapping = [[<C-\>]], -- Toggle with Ctrl+\
+      direction = "float", -- Use floating terminal
+      size = 20, -- Height for float/horizontal
+      start_in_insert = true, -- Enter insert mode on open
+      close_on_exit = true, -- Close terminal when process exits
+      hide_numbers = true, -- Hide line numbers
+      persist_size = true, -- Persist terminal size
+      persist_mode = true, -- Persist insert/normal mode
+      float_opts = {
+        border = "curved", -- Border style
+        width = math.floor(vim.o.columns * 0.9), -- 90% of window width
+        height = 20, -- Fixed height
+        winblend = 0, -- Opaque background
+        -- Position at the bottom
+        row = vim.o.lines - 20 - 3, -- Adjust for bottom placement (accounts for border/statusline)
+        col = math.floor((vim.o.columns - math.floor(vim.o.columns * 0.9)) / 2), -- Center horizontally
+        anchor = "SW", -- Anchor to bottom-left (South-West)
+      },
     },
   },
 }
