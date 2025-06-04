9bb4d982488bfc8bac7cc624d1bd805e76436f4c
rashed parvez <parvez08eee@gmail.com>
2025-06-04 23:05:28 +0600

    removed hover error popup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,9 +17,4 @@ return {
       end,
     },
   },
-  vim.api.nvim_create_autocmd({ "CursorHold" }, {
-    callback = function()
-      vim.diagnostic.open_float(nil, { focus = false })
-    end,
-  }),
 }
