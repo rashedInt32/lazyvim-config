bdd72cbc7ab066a670fd5946e0af3f9c0cfa74df
rashed parvez <parvez08eee@gmail.com>
2025-07-14 08:59:07 +0600

    save file with <c-w>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -77,6 +77,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- saving files
+vim.keymap.set({ "n", "i" }, "<C-w>", "<cmd>w<cr>", { desc = "Save file with <C-w>", noremap = true, silent = true })
+
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
