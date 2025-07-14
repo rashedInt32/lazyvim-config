3b2e73f6097f8c84268d8dc3dba8d46ea097b8a4
rashed parvez <parvez08eee@gmail.com>
2025-07-14 08:53:53 +0600

    Removed extras from toggleterms

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -14,21 +14,5 @@ return {
       persist_mode = true,
       autochdir = true,
     },
-    config = function(_, opts)
-      require("toggleterm").setup(opts)
-      vim.api.nvim_create_autocmd("TermOpen", {
-        pattern = "term://*toggleterm#*",
-        callback = function()
-          vim.opt_local.mouse = ""
-          local opts = { buffer = 0, noremap = true, silent = true }
-          vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
-          vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-          vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
-          vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
-          vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
-          vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
-        end,
-      })
-    end,
   },
 }
