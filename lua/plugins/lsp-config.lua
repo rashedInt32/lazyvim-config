71e2a57148ce711c72280dc7b62897b4b455f7f8
rashed parvez <parvez08eee@gmail.com>
2025-08-04 17:44:13 +0600

    This setup not working as expected

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,111 +18,98 @@ return {
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
     },
-    opts = {
-      inlay_hints = { enabled = false },
-      document_highlight = { enabled = false },
-      diagnostics = {
-        virtual_text = false,
-        signs = true,
-        underline = true,
-        float = {
-          focusable = false,
-          style = "minimal",
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
-        },
-      },
-      servers = {
-        vtsls = {
-          filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-          root_dir = require("lspconfig.util").root_pattern("package.json", "tsconfig.json", ".git"),
-          settings = {
-            typescript = {
-              inlayHints = {
-                parameterNames = { enabled = "literals" },
-                parameterTypes = { enabled = true },
-                variableTypes = { enabled = false },
-                propertyDeclarationTypes = { enabled = true },
-                functionLikeReturnTypes = { enabled = true },
-                enumMemberValues = { enabled = true },
-              },
+    config = function()
+      -- Disable signature help popups
+      vim.lsp.handlers["textDocument/signatureHelp"] = function() end
+
+      -- Setup mason
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+
+      -- Get capabilities (with fallback)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      if ok then
+        capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+      end
+
+      -- Common on_attach function
+      local on_attach = function(client, bufnr)
+        client.server_capabilities.signatureHelpProvider = false
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover (type info)" })
+        vim.keymap.set(
+          "n",
+          "<leader>D",
+          vim.lsp.buf.type_definition,
+          { buffer = bufnr, desc = "Go to type definition" }
+        )
+      end
+
+      -- Configure servers
+      local lspconfig = require("lspconfig")
+
+      -- vtsls configuration (will auto-start for TS/JS files)
+      lspconfig.vtsls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        root_dir = function()
+          return vim.loop.cwd()
+        end,
+        single_file_support = true,
+        settings = {
+          typescript = {
+            inlayHints = {
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              variableTypes = { enabled = false },
+              propertyDeclarationTypes = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              enumMemberValues = { enabled = true },
             },
           },
         },
-        tailwindcss = {
-          filetypes = {
-            "html",
-            "css",
-            "scss",
-            "javascript",
-            "typescript",
-            "javascriptreact",
-            "typescriptreact",
-            "svelte",
-            "vue",
-          },
-          root_dir = require("lspconfig.util").root_pattern(
-            "tailwind.config.js",
-            "tailwind.config.ts",
-            "postcss.config.js",
-            "package.json",
-            ".git"
-          ),
-        },
-        lua_ls = {},
-      },
-      setup = {
-        -- Just set on_attach here, no manual setup call
-        vtsls = function(_, opts)
-          opts.on_attach = function(client, bufnr)
-            client.server_capabilities.signatureHelpProvider = false
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover (type info)" })
-            vim.keymap.set(
-              "n",
-              "<leader>D",
-              vim.lsp.buf.type_definition,
-              { buffer = bufnr, desc = "Go to type definition" }
-            )
-          end
-        end,
-        ["*"] = function(_, opts)
-          opts.on_attach = function(client, bufnr)
-            client.server_capabilities.signatureHelpProvider = false
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover (type info)" })
-            vim.keymap.set(
-              "n",
-              "<leader>D",
-              vim.lsp.buf.type_definition,
-              { buffer = bufnr, desc = "Go to type definition" }
-            )
-          end
-        end,
-      },
-    },
-    config = function(_, opts)
-      -- Disable all automatic signature help popups globally
-      vim.lsp.handlers["textDocument/signatureHelp"] = function() end
-
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "vtsls", "tailwindcss" },
       })
 
-      -- Manually setup vtsls with options from opts.servers.vtsls
-      local lspconfig = require("lspconfig")
-      if opts.servers.vtsls then
-        -- Inject on_attach from opts.setup.vtsls if present
-        local vtsls_opts = vim.tbl_deep_extend("force", opts.servers.vtsls, {})
-        if opts.setup.vtsls then
-          opts.setup.vtsls(nil, vtsls_opts)
-        end
-        lspconfig.vtsls.setup(vtsls_opts)
-      end
+      -- TailwindCSS configuration
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+          "vue",
+        },
+        root_dir = require("lspconfig.util").root_pattern(
+          "tailwind.config.js",
+          "tailwind.config.ts",
+          "postcss.config.js",
+          "package.json",
+          ".git"
+        ),
+      })
 
-      -- You can still let LazyVim handle other servers automatically
+      -- LuaLS configuration
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+            telemetry = { enable = false },
+          },
+        },
+      })
     end,
   },
 }
