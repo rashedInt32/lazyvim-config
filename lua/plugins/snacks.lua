2f9dd6164dab326a9f61d2eae7a240cff37f9418
rashed parvez <parvez08eee@gmail.com>
2025-10-05 08:57:59 +0600

    Added snakcs terminal keymap to <c-\>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -573,20 +573,30 @@ return {
         end,
         desc = "Dismiss All Notifications",
       },
-      {
-        "<c-/>",
-        function()
-          Snacks.terminal()
-        end,
-        desc = "Toggle Terminal",
-      },
-      {
-        "<c-_>",
-        function()
-          Snacks.terminal()
-        end,
-        desc = "which_key_ignore",
-      },
+       {
+         "<c-/>",
+         function()
+           Snacks.terminal()
+         end,
+         desc = "Toggle Terminal",
+         mode = { "n", "t" },
+       },
+       {
+         "<c-_>",
+         function()
+           Snacks.terminal()
+         end,
+         desc = "which_key_ignore",
+         mode = { "n", "t" },
+       },
+       {
+         "<c-\\>",
+         function()
+           Snacks.terminal()
+         end,
+         desc = "Toggle Terminal",
+         mode = { "n", "t" },
+       },
       {
         "]]",
         function()
