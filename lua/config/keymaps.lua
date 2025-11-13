67c93bb556c4523026e93fc456f262d99403c4ce
rashed parvez <parvez08eee@gmail.com>
2025-11-13 08:13:08 +0600

    use lsp code action menu instead of tiny code action

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -137,10 +137,10 @@ end, { expr = true })
 
 vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>")
 
--- vim.keymap.set("n", "<leader>ca", function()
---   vim.lsp.buf.code_action()
--- end, { noremap = true, silent = true })
---
+vim.keymap.set("n", "<leader>ca", function()
+  vim.lsp.buf.code_action()
+end, { noremap = true, silent = true })
+
 -- vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Show signature help" })
 -- vim.keymap.set("n", "<leader><leader>", function()
 --     vim.cmd("so")
