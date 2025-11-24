2be6921a3bd1b5dd54295045bfec2656923bfe95
rashed parvez <parvez08eee@gmail.com>
2025-11-24 23:43:51 +0600

    fixed indent paste issue

---
+++ lua/plugins/yanky.lua
@@ -0,0 +1,36 @@
+return {
+  "gbprod/yanky.nvim",
+  opts = {
+    ring = {
+      storage = "shada",
+      ignore_registers = { "_" },
+    },
+    system_clipboard = {
+      sync_with_ring = true,
+    },
+    highlight = {
+      on_put = true,
+      on_yank = true,
+      timer = 200,
+    },
+  },
+  keys = {
+    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
+    { "p", "=p", mode = "n", remap = true, desc = "Put after with auto-indent" },
+    { "P", "=P", mode = "n", remap = true, desc = "Put before with auto-indent" },
+    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
+    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
+    { "[y", "<Plug>(YankyCycleForward)", desc = "Cycle forward through yank history" },
+    { "]y", "<Plug>(YankyCycleBackward)", desc = "Cycle backward through yank history" },
+    { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+    { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+    { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+    { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+    { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
+    { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
+    { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
+    { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
+    { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
+    { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
+  },
+}
