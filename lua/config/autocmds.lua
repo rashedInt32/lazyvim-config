cd56fd06ddd24bd45bbfe52c20c5b3803df8fdee
rashed parvez <parvez08eee@gmail.com>
2026-01-03 08:28:03 +0600

    Bring back diagnostic icos

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -145,7 +145,8 @@ vim.api.nvim_create_user_command("TestLualine", function()
     local dia = {
       "diagnostics",
       sources = { "nvim_diagnostic" },
-      symbols = { error = " ", warn = " ", info = " ", hint = " " },
+      symbols = { error = " ", warn = " ", info = " ", hint = " " },
+
       diagnostics_color = {
         error = { fg = colors.red },
         warn = { fg = colors.yellow },
