e31c16c5fe50c9ec92675d262a16796195880c6e
rashed parvez <parvez08eee@gmail.com>
2026-01-20 12:43:34 +0600

    Update comment color

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -26,7 +26,7 @@ return {
         bg = "#021320",
       }
       hl.Visual = {
-        bg = "#2d3f5f",
+        bg = "#1d3b53",
       }
       hl.CopilotSuggestion = {
         fg = "#5a6a8a",
@@ -34,8 +34,12 @@ return {
       hl.BlinkCmpGhostText = {
         fg = "#5a6a8a",
       }
+      -- hl.Comment = {
+      --   fg = "#637777",
+      --   italic = true,
+      -- }
       hl.Comment = {
-        fg = "#6772a4",
+        fg = "#6a8080",
         italic = true,
       }
     end,
@@ -47,12 +51,15 @@ return {
     vim.api.nvim_create_autocmd("ColorScheme", {
       pattern = "tokyonight",
       callback = function()
-        vim.api.nvim_set_hl(0, "Visual", { bg = "#2d3f5f" })
+        vim.api.nvim_set_hl(0, "Visual", { bg = "#1d3b53" })
         vim.api.nvim_set_hl(0, "CursorLine", { bg = "#021320" })
         vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#5a6a8a" })
         vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { fg = "#5a6a8a" })
-        vim.api.nvim_set_hl(0, "Comment", { fg = "#6772a4", italic = true })
+        --vim.api.nvim_set_hl(0, "Comment", { fg = "#637777", italic = true })
+        vim.api.nvim_set_hl(0, "Comment", { fg = "#6a8080", italic = true })
       end,
     })
   end,
 }
+
+-- This is previous comment color
