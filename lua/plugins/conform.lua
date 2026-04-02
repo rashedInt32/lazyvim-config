617366ac2e4324accca72467512025ac8edb4bc0
rashed parvez <parvez08eee@gmail.com>
2026-04-02 10:44:49 +0600

    disabled conform formatting for ts

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,19 +2,25 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      javascript = { "prettier", "biome" },
-      typescript = { "prettier", "biome" },
-      javascriptreact = { "prettier", "biome" },
-      typescriptreact = { "prettier", "biome" },
-      blade = { "blade-formatter" }, -- Add this line
+      javascript = { "prettier" },
+      --typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      --typescriptreact = { "prettier" },
+      blade = { "blade-formatter" },
       prisma = { "prisma" },
       lua = { "stylua" },
-      fish = {}, -- Disable Fish formatting
+      fish = {},
       sql = { "sleek" },
-      -- fallback
       ["_"] = { "trim_whitespace" },
     },
     formatters = {
+      stylua = {
+        command = "stylua",
+        filter = function(buf)
+          local filename = vim.api.nvim_buf_get_name(buf)
+          return not filename:match("autocmds%.lua$")
+        end,
+      },
       sleek = {
         command = "/Users/rashed/.cargo/bin/sleek",
         args = { "--indent-spaces=2", "--lines-between-queries=3" },
