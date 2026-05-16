b95664b2002dcea57acbe2acf5066ea0afa9498c
rashed parvez <parvez08eee@gmail.com>
2026-05-16 09:19:02 +0600

    update plugins

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,9 +1,8 @@
 return {
   "kdheepak/lazygit.nvim",
   cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
-  keys = {
-    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-  },
+  -- <leader>gg intentionally handled by Snacks.lazygit() (see snacks.lua) so that
+  -- pressing `e` opens the file in the previous window instead of a nested nvim.
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
