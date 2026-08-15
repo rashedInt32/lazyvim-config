a7288b094172ae0a2ba986222cb2afedf825ad76
rashed parvez <parvez08eee@gmail.com>
2026-08-15 08:46:06 +0600

    refactor(plugins): delete fully commented-out plugin files
    chat, code-action, eyeliner, lsp-signature, snippets and toggleterm were
    100% comments and parsed to empty tables. Also drop the WinLeave handler
    in config/autocmds.lua that closed floating toggleterm windows; it was
    gated behind a toggleterm_loaded flag that could never be set.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,31 +1,5 @@
 -- ~/.config/nvim/lua/autocmd.lua
 
--- Close floating terminals safely (only when toggleterm is loaded)
-local toggleterm_loaded = false
-vim.api.nvim_create_autocmd("User", {
-  pattern = "LazyLoad",
-  callback = function(args)
-    if args.data == "toggleterm.nvim" then
-      toggleterm_loaded = true
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "WinLeave" }, {
-  pattern = "*",
-  callback = function()
-    if not toggleterm_loaded then
-      return
-    end
-    local toggleterm = require("toggleterm.terminal")
-    for _, term in pairs(toggleterm.get_all()) do
-      if term.direction == "float" and vim.api.nvim_win_is_valid(term.window) then
-        term:close()
-      end
-    end
-  end,
-})
-
 vim.api.nvim_create_autocmd("User", {
   pattern = "BlinkLoaded",
   callback = function()
